e68a02346303fde00a8cec165d76fa555cb5665f
ryleeguy <112982301+ryleeguy@users.noreply.github.com>
2022-09-13 13:01:37 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,50 @@
-input.onGesture(Gesture.Shake, function () {
-    random_number = randint(0, 2)
-    if (random_number == 0) {
-        basic.showString("answer unclear")
-    } else if (random_number == 1) {
-        basic.showString("no")
-    } else {
-        basic.showString("yes")
+input.onButtonPressed(Button.A, function () {
+    cat = randint(0, 1)
+    if (cat == 0) {
+        basic.showString("true")
+    } else if (cat == 1) {
+        basic.showString("dare")
     }
 })
-let random_number = 0
-basic.showString("8 ball")
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    dog = randint(0, 4)
+    if (dog == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (dog == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (dog == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (dog == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    basic.pause(100)
+    basic.clearScreen()
 })
+let dog = 0
+let cat = 0
+basic.showString("t or d")
